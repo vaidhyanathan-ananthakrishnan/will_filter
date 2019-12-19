8200a4be01cba818f5e78692a5be9d7beef754f6
Vaidhy <vaidhyanathan.ananthakrishnan@freshworks.com>
2019-12-19 20:32:42 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 #--
-# Copyright (c) 2010-2011 Michael Berkovich
+# Copyright (c) 2017 Michael Berkovich, theiceberk@gmail.com
+#
+#  __    __  ____  _      _          _____  ____  _     ______    ___  ____
+# |  |__|  ||    || |    | |        |     ||    || |   |      |  /  _]|    \
+# |  |  |  | |  | | |    | |        |   __| |  | | |   |      | /  [_ |  D  )
+# |  |  |  | |  | | |___ | |___     |  |_   |  | | |___|_|  |_||    _]|    /
+# |  `  '  | |  | |     ||     |    |   _]  |  | |     | |  |  |   [_ |    \
+#  \      /  |  | |     ||     |    |  |    |  | |     | |  |  |     ||  .  \
+#   \_/\_/  |____||_____||_____|    |__|   |____||_____| |__|  |_____||__|\_|
+#
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -22,22 +31,23 @@
 #++
 
 WillFilter::Engine.routes.draw do
-  match 'filter/index', :to => 'filter#index'
-  match 'filter/add_condition', :to => 'filter#add_condition'
-  match 'filter/update_condition', :to => 'filter#update_condition'
-  match 'filter/remove_condition', :to => 'filter#remove_condition'
-  match 'filter/remove_all_conditions', :to => 'filter#remove_all_conditions'
-  match 'filter/load_filter', :to => 'filter#load_filter'
-  match 'filter/save_filter', :to => 'filter#save_filter'
-  match 'filter/update_filter', :to => 'filter#update_filter'
-  match 'filter/delete_filter', :to => 'filter#delete_filter'
 
-  match 'calendar', :to => 'calendar#index'
-  match 'calendar/index', :to => 'calendar#index'
+  match 'filter/index', :to => 'filter#index', via: [:get, :post]
+  match 'filter/add_condition', :to => 'filter#add_condition', via: [:get, :post]
+  match 'filter/update_condition', :to => 'filter#update_condition', via: [:get, :post]
+  match 'filter/remove_condition', :to => 'filter#remove_condition', via: [:get, :post]
+  match 'filter/remove_all_conditions', :to => 'filter#remove_all_conditions', via: [:get, :post]
+  match 'filter/load_filter', :to => 'filter#load_filter', via: [:get, :post]
+  match 'filter/save_filter', :to => 'filter#save_filter', via: [:get, :post]
+  match 'filter/update_filter', :to => 'filter#update_filter', via: [:get, :post]
+  match 'filter/delete_filter', :to => 'filter#delete_filter', via: [:get, :post]
 
-  match 'exporter', :to => 'exporter#index'
-  match 'exporter/index', :to => 'exporter#index'
-  match 'exporter/export', :to => 'exporter#export'
+  match 'calendar', :to => 'calendar#index', via: [:get, :post]
+  match 'calendar/index', :to => 'calendar#index', via: [:get, :post]
 
-  root :to => "filter#index"
+  match 'exporter', :to => 'exporter#index', via: [:get, :post]
+  match 'exporter/index', :to => 'exporter#index', via: [:get, :post]
+  match 'exporter/export', :to => 'exporter#export', via: [:get, :post]
+
+  root :to => 'filter#index'
 end
